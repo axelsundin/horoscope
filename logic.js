3fca06c7258c42a7fff59318767930ac6d18d3e8
axelsundin <axelsundin@hotmail.com>
2020-12-10 11:13:41 +0100

    added js, and some html structure

---
+++ logic.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", initSite)
+
+document.getElementById("dateInput")
+document.getElementById("saveBtn").addEventListener("click", saveDate)
+document.getElementById("updateBtn").addEventListener("click", updateDate)
+document.getElementById("deleteBtn").addEventListener("click", deleteDate)
+document.getElementById("outputDiv")
+
+function initSite() {
+
+}
+
+function updateDate() {
+
+}
+
+function initSite() {
+
+}
+
+function deleteDate() {
+
+}
