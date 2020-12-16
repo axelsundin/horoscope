45e94a6ef3eacca8ec5eddaa64c44fdd752d07ab
axelsundin <axelsundin@hotmail.com>
2020-12-16 09:37:07 +0100

    update

---
+++ php/calcHoroscope.php
@@ -0,0 +1,75 @@
+<?php
+
+session_start();
+
+function calcHoroscope($date) {
+    
+   if($date["month"] == 1 AND $date["day"] > 19
+   OR $date["month"] == 2 AND $date["day"] < 19) {
+
+     return "Vattumannen";
+
+   } elseif($date["month"] == 2 AND $date["day"] > 18
+   OR $date["month"] == 3 AND $date["day"] < 20) {
+
+      return "Fiskarna";
+
+   } elseif($date["month"] == 3 AND $date["day"] > 20
+   OR $date["month"] == 4 AND $date["day"] < 20) {
+
+      return "Väduren";
+
+   } elseif($date["month"] == 4 AND $date["day"] > 19
+   OR $date["month"] == 5 AND $date["day"] < 21) {
+
+      return "Oxen";
+
+   } elseif($date["month"] == 5 AND $date["day"] > 20
+   OR $date["month"] == 6 AND $date["day"] < 22) {
+
+      return "Tvillingarna";
+
+   } elseif($date["month"] == 6 AND $date["day"] > 21 
+   OR $date["month"] == 7 AND $date["day"] < 23) {
+
+      return "Kräftan";
+
+   } elseif($date["month"] == 7 AND $date["day"] > 22
+   OR $date["month"] == 8 AND $date["day"] < 23) {
+
+      return "Lejonet";
+
+   } elseif($date["month"] == 8 AND $date["day"] > 22
+   OR $date["month"] == 9 AND $date["day"] < 23) {
+
+      return "Jungfrun";
+
+   } elseif($date["month"] == 9 AND $date["day"] > 22
+   OR $date["month"] == 10 AND $date["day"] < 23) {
+
+      return "Vågen";
+ 
+   } elseif($date["month"] == 10 AND $date["day"] > 22
+   OR $date["month"] == 11 AND $date["day"] < 22) {
+
+      return "Skorpionen";
+
+   } elseif($date["month"] == 11 AND $date["day"] > 21
+   OR $date["month"] == 12 AND $date["day"] < 21) {
+
+      return "Skytten";
+
+   } elseif($date["month"] == 12 AND $date["day"] > 21
+   OR $date["month"] == 1 AND $date["day"] < 20) {
+
+      return "Stenbocken";
+
+   } else {
+
+      return "";
+
+   }
+    
+}
+
+?>
--- php/viewHoroscope.php
+++ php/viewHoroscope.php
@@ -1,8 +1,40 @@
 <?php
 
-function viewHoroscope() {
+try {
+
+    session_start();
+
+    if(isset($_SERVER["REQUEST_METHOD"])) {
+
+        if($_SERVER["REQUEST_METHOD"] === 'GET') {
+
+            if(isset($_SESSION["horoscope"])) {
+
+                echo json_encode(($_SESSION["horoscope"]));
+                exit;
+
+            } 
+            
+            else {
+
+                echo json_encode("");
+                exit;
+
+            }
+
+        }
+        
+    }
     
+} catch(Exception $error) {
+
+    echo json_encode( 
+        array(
+            "Message" => $error -> getMessage(),
+            "Status" => $error -> getCode()
+        ) 
+    );
+
 }
 
- 
 ?>
